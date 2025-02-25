1c076b6347212f722d3c3a21a1a881391e648c72
Nataly505 <dmitrievan730@gmail.com>
2025-02-25 16:51:43 +0500

    Добавила покраску победных значений в клетках в красный

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ let isWin = false;
 startGame();
 addResetListener();
 
-function startGame () {
+function startGame() {
     renderGrid(3);
 }
 
-function renderGrid (dimension) {
+function renderGrid(dimension) {
     container.innerHTML = '';
 
     for (let i = 0; i < dimension; i++) {
@@ -65,50 +65,67 @@ function cellClickHandler(row, col) {
 function checkWinner() {
     const size = field.length;
 
+    // Проверка строк
     for (let i = 0; i < size; i++) {
         if (field[i][0] && field[i][0] === field[i][1] && field[i][0] === field[i][2]) {
+            highlightCells([[i, 0], [i, 1], [i, 2]]);
             return field[i][0];
         }
+    }
+
+    // Проверка столбцов
+    for (let i = 0; i < size; i++) {
         if (field[0][i] && field[0][i] === field[1][i] && field[0][i] === field[2][i]) {
+            highlightCells([[0, i], [1, i], [2, i]]);
             return field[0][i];
         }
     }
 
+    // Проверка диагоналей
     if (field[0][0] && field[0][0] === field[1][1] && field[0][0] === field[2][2]) {
-        return field[0][0]; 
+        highlightCells([[0, 0], [1, 1], [2, 2]]);
+        return field[0][0];
     }
     if (field[0][2] && field[0][2] === field[1][1] && field[0][2] === field[2][0]) {
-        return field[0][2]; 
+        highlightCells([[0, 2], [1, 1], [2, 0]]);
+        return field[0][2];
     }
 
     return null;
 }
 
-function renderSymbolInCell (symbol, row, col, color = '#333') {
+function highlightCells(cells) {
+    cells.forEach(([row, col]) => {
+        const cell = findCell(row, col);
+        cell.style.color = 'red';
+    });
+}
+
+function renderSymbolInCell(symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
 
     targetCell.textContent = symbol;
     targetCell.style.color = color;
 }
 
-function findCell (row, col) {
+function findCell(row, col) {
     const targetRow = container.querySelectorAll('tr')[row];
     return targetRow.querySelectorAll('td')[col];
 }
 
-function addResetListener () {
+function addResetListener() {
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
 }
 
-function resetClickHandler () {
+function resetClickHandler() {
     console.log('reset!');
 }
 
 
 /* Test Function */
 /* Победа первого игрока */
-function testWin () {
+function testWin() {
     clickOnCell(0, 2);
     clickOnCell(0, 0);
     clickOnCell(2, 0);
@@ -119,7 +136,7 @@ function testWin () {
 }
 
 /* Ничья */
-function testDraw () {
+function testDraw() {
     clickOnCell(2, 0);
     clickOnCell(1, 0);
     clickOnCell(1, 1);
@@ -132,6 +149,6 @@ function testDraw () {
     clickOnCell(2, 2);
 }
 
-function clickOnCell (row, col) {
+function clickOnCell(row, col) {
     findCell(row, col).click();
 }
